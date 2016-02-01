3b6813d1a7f2c910cea46ca481a62d052ecb240b
xiechengjian <xiechengjian1994@163.com>
2016-02-01 20:09:41 +0800

    2/1

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ $.extend(Action.prototype ,{
         elementHeight:115,
         sprite1:new Image(),//items，基本图
         sprite2:new Image(),//元素，特效图
+        pathImage:new Image(),
+        pathFlash:new Image(),//路径特效图
+        boom:new Image(),//绘制爆炸
     },
     //初始化
     init:function(){
@@ -25,6 +28,8 @@ $.extend(Action.prototype ,{
         var ctx=canvas.getContext('2d');
         self.config.sprite1.src="sprite1.png";
         self.config.sprite2.src="sprite2.png";
+        self.config.pathImage.src="path.png";
+        self.config.boom.src="boom.png";
         var trans=self.config.rootSize;
         self.config.sprite2.onload=function(){
             var ePoint=new Point(50,50);
@@ -92,6 +97,7 @@ $.extend(Action.prototype ,{
         setTimeout(function(){//画Items
                 ctx.drawImage(sprite1,iPoint.x,iPoint.y,70,70,ePoint.x+6,ePoint.y+6,70*trans,70*trans);
             },600);
+        return ;
     },
     canvasTouch:function(ctx,e,self){
         //var self=this;
@@ -110,9 +116,53 @@ $.extend(Action.prototype ,{
             setTimeout(function(){
                 ctx.clearRect(50,50,eHeight,eWidth);
                 ctx.drawImage(sprite2,95,0,eWidth,eHeight,50,50,eWidth*trans,eHeight*trans);
-                ctx.drawImage(sprite1,0,0,70,70,50+6,50+6,70*trans,70*trans);
+                ctx.drawImage(sprite1,0,430,70,70,50+6,50+6,70*trans,70*trans);
             },200);
         }
+        self.drawPath(ctx);
+        ePoint=new Point(50,50);
+        self.drwaBoom(ctx,ePoint);
+    },
+    drawPath:function(ctx){
+        var self=this;
+        var eHeight=self.config.elementHeight,
+            eWidth=self.config.elementWidth,
+            sprite2=self.config.sprite2,
+            sprite1=self.config.sprite1,
+            trans=self.config.rootSize,
+            pathFlash=self.config.pathFlash,
+            pathImage=self.config.pathImage;
+            //ctx.drawImage(pathFlash,0,0,110,79,0,0,110*trans*.4,79*trans*.4);
+            //ctx.drawImage(pathFlash,0,0,110,79,110*trans*.4,0,110*trans*.4,79*trans*.4);
+            // var time=0;
+            // while(time<10){
+            //     ctx.drawImage(pathFlash,0,0,128,128,128*trans*.6*time,8,128*trans*.6,128*trans*.6);
+            //     time++;
+            // }
+            var texture=ctx.createPattern(pathImage,"repeat");
+            ctx.strokeStyle=texture;
+            ctx.moveTo(0,20);
+            ctx.lineTo(400,20);
+            ctx.lineWidth=3;
+            ctx.stroke();   
+    },
+    drwaBoom:function(ctx,ePoint){
+        var self=this;
+        var eHeight=self.config.elementHeight,
+            eWidth=self.config.elementWidth,
+            boom=self.config.boom,
+            trans=self.config.rootSize;
+             //ctx.clearRect(ePoint.x,ePoint.y,eHeight,eWidth);
+            setTimeout(function(){
+                //ctx.scale(.7,.7);
+                ctx.clearRect(ePoint.x,ePoint.y,eHeight,eWidth);
+                //ctx.drawImage(boom,0,120,125,135,ePoint.x,ePoint.y,125*trans,125*trans);
+            },250);
+            // setTimeout(function(){
+            //     ctx.clearRect(ePoint.x,ePoint.y,eHeight,eWidth);
+            //     ctx.scale(10/7,10/7);
+            //     ctx.drawImage(boom,0,120,125,135,ePoint.x,ePoint.y,125*trans,125*trans);
+            // },400);
     }
 });
  new Action().init();
